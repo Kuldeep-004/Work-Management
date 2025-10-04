5987a1f32e18b29d3efe39c952c10a11ae787116
Kuldeep <kalariyakuldeep8238@gmail.com>
2025-10-04 10:15:13 +0530

    Commpleted automation requirement

--- backend/server.js
+++ backend/server.js
@@ -438,3 +438,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
+
+app.get('/', (req, res) => {
+  res.send('<h1>HAACAS Server Is Running</h1>');
+});
